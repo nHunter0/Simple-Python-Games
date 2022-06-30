a45b68a7f58e233fbf389bf74a7b4bc87fcb8685
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2022-06-30 15:09:52 +1000

    Commit 2

--- Casino.py
+++ Casino.py
@@ -2,11 +2,18 @@ from time import sleep
 import random
 import os 
 
+"""
+    TODO: 
+     - Add game_data file to save users data after game conclution
+     - SPEEL CHECK ! 
+     - Verify type(user inputs ) == int 
+"""
+
 def age_verification():
     user_age = int(input("Please input your age to continue: "))
     if user_age < 18:
         playable_age = abs(18 - user_age) 
-        print("Why are you gambaling at age " + str(user_age) + "...") 
+        print("Why are you gambling at age " + str(user_age) + "...") 
         print("Please leave and return when your 18 years of age in " + str(playable_age) + " years. Thank you")
         sleep(5)
         exit()
@@ -19,10 +26,8 @@ def obtain_data():
     return name, money
 
 def print_data(name, money):
-    print("---Infomation---")
     print("Hello, " + name)
     print("You have " + str(money) + "$ to play with")
-
 
 def play_game(money):
     starting_money = money
@@ -42,7 +47,7 @@ def play_game(money):
 
     while (playing == True):
         #Main Game
-        play_money = int(input("How much would you like to gamble with: "))
+        play_money = int(input("Out of your " + money + "$ how much would you like to gamble with:"))
         dice_roll = random.randint(1,6)
 
         if(play_money > int(money) or play_money == 0):
@@ -57,34 +62,39 @@ def play_game(money):
             print(dice_roll)
             
             if(dice_roll == user_guess ):
-                won_money = play_money * 2
+                won_money = play_money * 2 
                 money = int(money) + won_money
-                print("Wow congradulation! you have won " + str(won_money) + "$")
+                print("Wow congradulations! you have won " + str(won_money) + "$")
             else:
-                print("Yikes you lost ):")
+                print("Yikes you lost :(")
                 money = int(money) - play_money
                 print("You have lost " + str(play_money) + "$") 
         
         if(int(money) == 0 ):
             print("Dammmm looks like you have 0$")
             print("Thanks for the " + str(starting_money) + "$ and come back next time")
+            #TODO: DELETE game_data file HERE !!
             sleep(5)
             exit()
 
         #Continue playing 
         print("You now have : " + str(money) + "$")
-        again = input("Would you like to continue to play ? (y/n): ")
-        if(again == "y"):
+        play_again = input("Would you like to continue to play ? (y/n): ")
+        if(play_again == "y" or play_again == "Y"):
             pass
-        if(again == "n"):
+        if(play_again == "n" or play_again == "N"):
             playing = False
             os.system("cls")
-            print("---Resaults---")
+            print("---Results---")
             print("You srated with " + str(starting_money) + "$ and now have " + str(money) + "$")
             print("Thank you for playing!")
             sleep(5)
             exit()
+        else: 
+            print("invalid input")
+            pass # if user_input =! 'y' or 'n' -> continue playing
 
+#Main 
 def run_casino():
     print("---Welcome---")
     age_verification()
